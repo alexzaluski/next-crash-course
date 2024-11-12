f786315c2a294bcf9b6ac13b8c0a257b64adeed9
alexzaluski <aleksander.zal@gmail.com>
2024-11-12 16:51:18 +0100

    Show post excerpt isntead of body in the post list

--- components/PostItem.jsx
+++ components/PostItem.jsx
@@ -5,7 +5,7 @@ const PostItem = ({ post }) => {
   return (
     <Link href={`/posts/${post.id}`} className={styles.card}>
       <h3>{post.title}</h3>
-      <p>{post.body}</p>
+      <p>{post.excerpt}</p>
     </Link>
   );
 };
