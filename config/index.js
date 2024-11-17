965241a05d23cbf8337f62a7480ffb3630d6bdec
alexzaluski <aleksander.zal@gmail.com>
2024-11-17 18:32:25 +0100

    Added port handling from process.env file (removed hardcoded port 3000)

--- config/index.js
+++ config/index.js
@@ -1,6 +1,9 @@
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== "production";
+
+// Use process.env.PORT or default to 3000
+const port = process.env.PORT || 3000;
 
 export const server = dev
-  ? 'http://localhost:3000'
-  : 'https://some_website.com';
+  ? `http://localhost:${port}`
+  : "https://some_website.com";
 // : 'https://webdevnewz.vercel.app';
